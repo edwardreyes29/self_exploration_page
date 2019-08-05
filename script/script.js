6b98ee93098ab5255e7630614524e6a61fc3c5f8
Edward Reyes <reyes.edwardf@gmail.com>
2019-08-05 16:17:46 -0700

    Add modal click close option

--- script/script.js
+++ script/script.js
@@ -8,3 +8,27 @@ function clickFunction(clicked_id) {
         elementClassList[i].classList.add('clicked');
     }   
 }
+
+function clickClose(clicked_id) {
+    var element = document.getElementById(clicked_id);
+    console.log(element);
+    
+    console.log(element.id);
+    if (element.id == "close_1") {
+        var modalElement = document.getElementById('first_modal');
+        console.log(modalElement);
+        modalElement.classList.remove('clicked');
+    } else if (element.id == "close_2") {
+        var modalElement = document.getElementById('second_modal');
+        console.log(modalElement);
+        modalElement.classList.remove('clicked');
+    } else if (element.id == "close_3") {
+        var modalElement = document.getElementById('third_modal');
+        console.log(modalElement);
+        modalElement.classList.remove('clicked');
+    } else if (element.id == "close_4") {
+        var modalElement = document.getElementById('fourth_modal');
+        console.log(modalElement);
+        modalElement.classList.remove('clicked');
+    }
+}
