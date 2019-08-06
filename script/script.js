b336bf1cdda3a65bce965abf40123c7fa9fbd139
Edward Reyes <reyes.edwardf@gmail.com>
2019-08-06 16:01:22 -0700

    change question color

--- script/script.js
+++ script/script.js
@@ -4,7 +4,7 @@ function clickFunction(clicked_id) {
     var elementClassList = document.querySelectorAll("." + element.classList[0]);
 
     for (var i = 0; i < elementClassList.length; i++) {
-        console.log(elementClassList[i]); //
+        console.log(elementClassList[i]); 
         elementClassList[i].classList.add('clicked');
     }   
 }
