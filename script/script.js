171a05bb0f80f5e4a8f942e2051897ab990f0413
Edward Reyes <reyes.edwardf@gmail.com>
2019-08-26 12:49:46 -0700

    Add alt-text descriptions

--- script/script.js
+++ script/script.js
@@ -88,7 +88,6 @@ function arrowHoverOff() {
 
 function indicatorHover(indicator_id){
 
-    // Shortens Code!!!!!!!!
     var element = document.getElementById(indicator_id).getElementsByClassName('question_mark');
     element[0].classList.remove('revertColor');
     element[0].classList.add('changeColor');
@@ -100,44 +99,7 @@ function indicatorHover(indicator_id){
     var element3 = document.getElementById(indicator_id).getElementsByClassName('ask_yourself_text');
     console.log(element3);
     element3[0].classList.remove('showAsk');
-    element3[0].classList.add('hideAsk');
-
-    // if (indicator_id == 'question_indicator_1') {
-    //     // let element = document.getElementById('indicator_box_1');
-    //     // element.classList.remove('shrink');
-    //     // element.classList.add('expand');
-        
-    //     element = document.getElementById('ask_yourself_text_1');
-    //     element.classList.remove('showAsk');
-    //     element.classList.add('hideAsk');
-    // } else if (indicator_id == 'question_indicator_2') {
-    //     // let element = document.getElementById('indicator_box_2');
-    //     // element.classList.remove('shrink');
-    //     // element.classList.add('expand');
-
-    //     element = document.getElementById('ask_yourself_text_2');
-    //     element.classList.remove('showAsk');
-    //     element.classList.add('hideAsk');
-
-        
-    //     // element = document.getElementById('question_mark_2');
-    //     // element.classList.remove('revertColor');
-    //     // element.classList.add('changeColor');
-    // } else if (indicator_id == 'question_indicator_3') {
-    //     // let element = document.getElementById('indicator_box_3');
-    //     // element.classList.remove('shrink');
-    //     // element.classList.add('expand');
-    // } else if (indicator_id == 'question_indicator_4') {
-    //     // let element = document.getElementById('indicator_box_4');
-    //     // element.classList.remove('shrink');
-    //     // element.classList.add('expand');
-
-    //     element = document.getElementById('ask_yourself_text_4');
-    //     element.classList.remove('showAsk');
-    //     element.classList.add('hideAsk');
-    // }
-
-    
+    element3[0].classList.add('hideAsk');  
 }
 function indicatorHoverOff(indicator_id){
 
@@ -153,40 +115,4 @@ function indicatorHoverOff(indicator_id){
     element = document.getElementById(indicator_id).getElementsByClassName('ask_yourself_text');
     element[0].classList.remove('hideAsk');
     element[0].classList.add('showAsk');
-   
-
-    // if (indicator_id == 'question_indicator_1') {
-    //     let element = document.getElementById('indicator_box_1');
-    //     // element.classList.remove('expand');
-    //     // element.classList.add('shrink');
-
-    //     element = document.getElementById('ask_yourself_text_1');
-    //     element.classList.remove('hideAsk');
-    //     element.classList.add('showAsk');
-
-    // } else if (indicator_id == 'question_indicator_2') {
-    //     let element = document.getElementById('indicator_box_2');
-    //     // element.classList.remove('expand');
-    //     // element.classList.add('shrink');
-
-    //     element = document.getElementById('ask_yourself_text_2');
-    //     element.classList.remove('hideAsk');
-    //     element.classList.add('showAsk');
-
-    // } else if (indicator_id == 'question_indicator_3') {
-    //     let element = document.getElementById('indicator_box_3');
-    //     // element.classList.remove('expand');
-    //     // element.classList.add('shrink');
-    // } else if (indicator_id == 'question_indicator_4') {
-    //     let element = document.getElementById('indicator_box_4');
-    //     // element.classList.remove('expand');
-    //     // element.classList.add('shrink');
-
-    //     element = document.getElementById('ask_yourself_text_4');
-    //     element.classList.remove('hideAsk');
-    //     element.classList.add('showAsk');
-    // }
-
-    
-
 }
