063543deec35a8679452c9df1e1d006b762bd173
Edward Reyes <reyes.edwardf@gmail.com>
2019-09-25 10:06:34 -0700

    Shape animations set 9-25-19

--- self-exploration-final/script/script.js
+++ self-exploration-final/script/script.js
@@ -10,9 +10,9 @@ function clickFunction(clicked_id) {
     console.log(element.id);
     if (element.id == 'toggle-button') {
         document.getElementById('click_here_hover1').style.display = "none";
-    } else if (element.id == 'big_heart') { 
+    } else if (element.id == 'big-heart') { 
         document.getElementById('click_here_hover2').style.display = "none";
-    } else if (element.id == 'blue_triangle') { 
+    } else if (element.id == 'blue-triangle') { 
         document.getElementById('click_here_hover3').style.display = "none";
     }
 
